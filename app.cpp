66c600071eb2b884f92518a3c5da75556b854984
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2023-09-12 23:28:18 -0500

    third commit (main)

--- app.cpp
+++ app.cpp
@@ -1,17 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
-
-int sum(int arr[]){
-    int total =0;
-    for(int i =0; i <sizeof(arr)/sizeof(int);++i){
-        total+= arr[i];
+int sum(vector<int> vec){
+    int total = 0;
+    for(int i =0; i <vec.size();++i){
+        total+= vec[i];
     }
     return total;
 }
 
-int product(int arr[]){
+int product(vector<int> vec){
     int total =1;
-    for(int i =0; i <sizeof(arr)/sizeof(int);++i){
-        total= arr[i] * total;
+    for(int i =0; i <vec.size();++i){
+        vec[i] = vec[i] * total;
     }
     return total;
+}
+
+int main(int argc, char *argv[])
+{
+    vector<int> vec;
+    int num =0;
+    while(cin>>num){
+        vec.push_back(num);
+    }
+    int sumval = sum(vec);
+    int productval = product(vec);
+    cout<< "Sum: " << sumval <<endl;
+    cout<< "Product: " << productval <<endl;
+   
 }
